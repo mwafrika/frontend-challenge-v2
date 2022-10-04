023c214bf2b77eba7e3369eaea6334cd1b549b5e
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-10-04 13:21:19 +0200

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
+import NotFound from './components/notFound';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='App'>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' index element={<Home />} />
+          {/* <Route path='/dragons' element={<Dragon />} /> */}
+          <Route path='/not' element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='w-[375px] h-[813px] bg-[#F6F2FF] flex flex-col overflow-y-scroll pt-[94px]'>
+    <div className='w-[375px] h-[813px] bg-[#F6F2FF] flex flex-col overflow-y-scroll pt-[94px] overflow-x-hidden'>
       <div className='flex flex-col items-center p-0 w-[321px] h-[160px] font-[Helvetica] gap-4'>
         <h1 className='text-[36px] leading-[41px] text-center font-bold not-italic text-[#240D57]'>
           Imagine if
@@ -25,9 +26,50 @@ function App() {
           className='w-[165.63px] h-[292px] mx-auto'
         />
       </div>
-      <button className='py-[16px] px-[21px] text-[16px] text-[#FFFFFF] leading-[18px] font-bold'>
+      <button
+        className='w-[187px] h-[50px] text-[16px] text-[#FFFFFF] leading-[18px] font-bold bg-gradient-to-r to-[#E87BF8] from-[#8456EC] mx-auto flex justify-center items-center p-[16px] gap-[10px] rounded-[10px] mb-[13.7px]
+      hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out
+      '
+      >
         🎉 Create my event
       </button>
+      <div className=' w-[375px] h-[0.5px] bg-gray-300 mb-2'></div>
+      <div className='w-[331px] h-[44px] bg-gray-300 rounded-xl mx-auto flex items-center justify-between px-3 text-[14px] leading-[17px] mb-[14px]'>
+        <img src='/a.svg' alt='a' />
+        <div className='flex gap-1'>
+          <img src='/lock.svg' alt='lock' />
+          <p>domain.com</p>
+        </div>
+        <img src='/reload.svg' alt='reload' />
+      </div>
+
+      <ul className='flex w-[375px] h-[44px] justify-between items-center px-7'>
+        <li>
+          <Link to=''>
+            <img src='/bottom/left-color.svg' alt='left' />
+          </Link>
+        </li>
+        <li>
+          <Link to=''>
+            <img src='/bottom/right.svg' alt='right' />
+          </Link>
+        </li>
+        <li>
+          <Link to=''>
+            <img src='/bottom/upload.svg' alt='upload' />
+          </Link>
+        </li>
+        <li>
+          <Link to=''>
+            <img src='/bottom/square.svg' alt='square' />
+          </Link>
+        </li>
+        <li>
+          <Link to=''>
+            <img src='/bottom/book.svg' alt='book' />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
